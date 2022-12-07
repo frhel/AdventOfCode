3c5af415d33766c592fdbe418bd18d1be0b65a72
Freyr H <freyr.helga@gmail.com>
2022-12-07 12:58:53 +0000

    Day 7 Solution - Changed some naming from camelCase to snake_case Am trying to get used to writing in snake_case instead of camelCase

--- day_7.js
+++ day_7.js
@@ -9,16 +9,16 @@ const input = fs.readFileSync('./data/day_7', 'utf8').split('\r\n').reverse();
 
 // ------------------------------------------------------------------------------------------------
 // Build a tree structure from the input data
-let file_tree = buildTree(input);
+let file_tree = build_tree(input);
 
 // Calculate the total size of all files recursively including the size of all subdirectories
-file_tree = calcRecursiveDirSizes(file_tree);
+file_tree = calc_recursive_dir_sizes(file_tree);
 
 
 // ------------------------------------------------------------------------------------------------
 // Part 1: Find all directories with a size less than 100000 and sum their sizes
 let max_directory_size = 100000;
-let dir_size_list = findAllPathsWithSize(file_tree, max_directory_size, false);
+let dir_size_list = find_all_paths_with_size(file_tree, max_directory_size, false);
 
 // Total size of all directories with a size less than max_directory_size
 let total_result_size = dir_size_list.reduce((acc, val) => acc + val[1], 0);
@@ -29,23 +29,23 @@ console.log('Part 1 Solution: ' + total_result_size);
 // Part 2: Find the directory with the minimum required size to free up enough space for the update
 // Define the file system size, update size, and free space
 let [file_system_size, update_size] = [70000000, 30000000];
-let freeSpace = file_system_size - file_tree.total_size;
+let free_space = file_system_size - file_tree.total_size;
 
 // Find how much space we need to free up to make room for the update
-let spaceNeeded = update_size - freeSpace;
+let space_needed = update_size - free_space;
 
 // Find the directory with the minimum required size to free up enough space for the update
 // Sort the returned list by ascending size and return the first element
-let dir_to_delete = findAllPathsWithSize(file_tree, spaceNeeded)
+let dir_to_delete = find_all_paths_with_size(file_tree, space_needed)
                     .sort((a, b) => a[1] - b[1])
                     .at(0);
 console.log(`Part 2 Solution: ${dir_to_delete[1]}`);
 
 
 // ------------------------------------------------------------------------------------------------
-// fn: findAllPathsWithSize() - Find all directories with a size greater or less than the size
+// fn: find_all_paths_with_size() - Find all directories with a size greater or less than the size
 //     depending on the value of the min parameter and return a list of all paths and their sizes
-function findAllPathsWithSize(dir, size, min = true, path = []) {
+function find_all_paths_with_size(dir, size, min = true, path = []) {
     let dir_size_list = [];
     // loop through all keys in the current directory
     for (let key in dir) {
@@ -55,7 +55,7 @@ function findAllPathsWithSize(dir, size, min = true, path = []) {
         } else if (typeof dir[key] === 'object') {
             // if the key is a directory, recursively call the function to find all paths that meet
             // the size condition
-            dir_size_list = dir_size_list.concat(findAllPathsWithSize(dir[key], size, min, path.concat(key)));
+            dir_size_list = dir_size_list.concat(find_all_paths_with_size(dir[key], size, min, path.concat(key)));
         }
     }
     return dir_size_list;
@@ -71,9 +71,9 @@ function compare_size(size1, size2, min) {
 
 
 // ------------------------------------------------------------------------------------------------
-// fn: calcRecursiveDirSizes() - Calculate the total size of all files recursively including the 
+// fn: calc_recursive_dir_sizes() - Calculate the total size of all files recursively including the 
 //     size of all subdirectories and save the result as a 'total_size' property of each directory
-function calcRecursiveDirSizes(dir) {
+function calc_recursive_dir_sizes(dir) {
     let dir_size = 0;
     // loop through all keys in the current directory
     for (let key in dir) {
@@ -82,7 +82,7 @@ function calcRecursiveDirSizes(dir) {
         } else if (typeof dir[key] === 'object') {
             // if the key is a directory, recursively call the function to calculate the total size
             // of all files in the subdirectory and add the child directory size to the total size
-            dir[key] = calcRecursiveDirSizes(dir[key]);
+            dir[key] = calc_recursive_dir_sizes(dir[key]);
             dir_size += dir[key]['total_size'];
         }
     }
@@ -92,24 +92,24 @@ function calcRecursiveDirSizes(dir) {
 
 
 // ------------------------------------------------------------------------------------------------
-// fn: buildTree() - Build a tree structure from the input data
+// fn: build_tree() - Build a tree structure from the input data
 //    The tree structure is a nested object where each directory is a key in the object and the
 //    value is either an object representing a subdirectory or a number representing the size of
 //    a file
-function buildTree(input, tree = {}, path = []) {
+function build_tree(input, tree = {}, path = []) {
     // Get the current directory object from the tree by following the absolute path we are
     // currently on
-    let current_dir = getCurrentDirObject(path, tree);
+    let current_dir = get_current_dir_object(path, tree);
 
     // Grab the next line of input and split it on the space character 
     let line = input.pop().split(' ');
 
     if (line[0] === '$' && line[1] === 'cd') { // If the line is a cd command
         // Update the absolute path array
-        path = updatePath(path, line[2]);
+        path = update_path(path, line[2]);
 
         // Grab the new directory object from the tree by following the new absolute path
-        current_dir = getCurrentDirObject(path, tree);
+        current_dir = get_current_dir_object(path, tree);
 
     } else if (line[0] === '$' && line[1] === 'ls') { // If the line is an ls command
         // We're entering a loop to process all the files and directories for the current directory.
@@ -141,14 +141,14 @@ function buildTree(input, tree = {}, path = []) {
 
     // If we still have input, recursively call the function to process the next line of input
     if (input.length === 0) return tree;
-    else return buildTree(input, tree, path);
+    else return build_tree(input, tree, path);
 }
 
 
 // ------------------------------------------------------------------------------------------------
-// fn: getCurrentDirObject() - Get the object representing the current directory from the tree
+// fn: get_current_dir_object() - Get the object representing the current directory from the tree
 //     structure by following the absolute path from the root directory
-function getCurrentDirObject(path, tree) {
+function get_current_dir_object(path, tree) {
     let current_dir = tree;
     // Loop through the absolute path and follow the path to get the current directory object
     for (let dir of path) {
@@ -163,9 +163,9 @@ function getCurrentDirObject(path, tree) {
 
 
 // ------------------------------------------------------------------------------------------------
-// fn: updatePath() - Keep track of the current directory by updating the path array depending on
+// fn: update_path() - Keep track of the current directory by updating the path array depending on
 //     the current command
-function updatePath(path, dir) {
+function update_path(path, dir) {
     if (dir === '..') {
         path.pop();
     } else {
