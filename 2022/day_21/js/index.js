683d27b7aba6ac9c56e36a6aa3f438d5420e053a
frhel <freyr.helga@gmail.com>
2023-05-26 13:05:46 +0000

    2022 Day 21  - Combine part 1 and part 2 into one function call.   - Get the final answer for Part 1 and Part 2.

--- 2022/day_21/js/index.js
+++ 2022/day_21/js/index.js
@@ -27,31 +27,97 @@ _inverse = {
 
 // ---------------------------- Solution -------------------------------------
 const solution_start = performance.now();
+let answer = solve(input.slice(), node_name);
 // -------------------------------- Part 1 -----------------------------------
-const part1_start = performance.now();
-const part1_answer = solve_part1(input.slice(), node_name);
-console.log(`Part 1 answer: ${part1_answer} in ${(performance.now() - part1_start).toFixed(2)}ms`);
+console.log(`Part 1 answer: ${answer.part1}`);
 
 // -------------------------------- Part 2 -----------------------------------
-const part2_start = performance.now();
-const part2_answer = solve_part2(input.slice(), node_name);
-console.log(`Part 2 answer: ${part2_answer} in ${(performance.now() - part2_start).toFixed(2)}ms`);
+console.log(`Part 2 answer: ${answer.part2}`);
 
 console.log(`Solution completed in ${(performance.now() - solution_start).toFixed(2)}ms`);
 
 // ------------------------------ Main logic ----------------------------------
-                     /////////////////////////////////
-                     ////// PROBLEM AREA BEGINS //////
-                     /////////////////////////////////
+// Solve both parts of the problem in one go. The solve() function returns an
+// object with the answers for both parts.
+// Recursively builds a tree of objects from the input array. Each node in the
+// tree represents one line(monkey) from the input array. The tree is built
+// from the top down, starting with the root node. Each node has a name, a
+// number, an operation, and a left and right node. The left and right nodes
+// are objects with the same properties as the parent node. The tree is built
+// recursively by calling the solve() function with the name of the left and
+// right nodes as arguments. The solve() function returns the node object for
+// the current node. The solve() function also evaluates the operand of the
+// current node and assigns the result to the current node. The solve() function
+// also keeps track of whether the current branch of the tree has humn in it.
+// This is used later to solve part 2.
+// The solve() function returns the node object for the root node. The root
+// node has the answer for part 1 as its number property. The solve() function
+// also calls the track_human_path() function to get the answer for part 2.
+// The track_human_path() function traverses the tree back to the humn node
+// and reverses the operations to get the number that humn would have had to
+
+function solve(input, node_name) {
+   let curr_line = input[input.findIndex(line => line[0] === node_name)];
+
+   // Check if we have reached a leaf node. Leaf nodes have a length of 2
+   // node name and a value. Non-leaf nodes have a length of 4:
+   // node name, left node name, operation, right node name
+   let leaf = curr_line.length === 2;
+
+   // creating the node object for the current node of the tree
+   let node = {
+      name: node_name, 
+      num: leaf ? Number(curr_line[1]) : -Infinity,
+      operation: leaf ? '' : curr_line[2],
+      left: {name: leaf ? '' : curr_line[1]},
+      right: {name: leaf ? '' : curr_line[3]},
+      has_human: false
+   };
+
+   // If the node has a number, we have reached a leaf node and can return it.
+   if (node.num > -Infinity) {
+      // Gotta check if we have reached humn so we can track the path back to it later
+      if (node.name === 'humn') node.has_human = true;
+      return node;
+   } else {
+      // Recursive call to get the left and right nodes of the current node
+      node.left = solve(input, node.left.name);
+      node.right = solve(input, node.right.name);
+      
+      // use eval() to evaluate the operation and assign the resulting value to the current node
+      node.num = Number(eval(`${node.left.num} ${node.operation} ${node.right.num}`));
+
+      // Keep track of whether the current branch has humn in it
+      if (node.left.has_human || node.right.has_human) {
+         node.has_human = true;
+      }
+      
+      // If we're back to the root node, we can call track_human_path() to get the answer for part 2
+      // Or just return the value for Part 1. Since we can reuse the output from Part 1 for Part 2,
+      // we can just save the value for Part 1 in a return object, continue on solving for Part 2,
+      // and then return the object with both answers at the end.
+      if (node.name === 'root') {
+         // Save the part 1 answer
+         let answers = {part1: node.num}
+         
+         // Solve for part 2 with the tree object we have built
+         let nr_to_match = node.left.has_human ? node.right.num : node.left.num;
+         let humn_branch = node.left.has_human ? node.left : node.right;
+
+         // Save the result of track_human_path() in the return object and return it
+         answers.part2 = track_human_path(humn_branch, nr_to_match);
+         return answers;
+      }
+      return node
+   }
+}
+
+
 // This is where we traverse the tree back to humn and reverse the operations
-// to get the number that humn would have had to enter to get to the root node.
-// However, something is fucky and I'm getting very wrong results back.
-// It works fine for the sample input, but for some reason it just keeps returning
-// very incorrect results for the main dataset.
+// to get the number that humn would have had to enter to get to the root node
 function track_human_path(node, tracked_number) {
    // If we have reached the humn node, return the answer.
    if (node.name === 'humn') {
-      console.log(tracked_number);
       return tracked_number;
    }
 
@@ -59,104 +125,35 @@ function track_human_path(node, tracked_number) {
    let humn_side = node.left.has_human ? node.left : node.right;
    // console.log(node.left.has_human, node.left.num, node.operation, node.right.num, node.right.has_human)
 
-   // Check which node has the human and chuck the current evaluated number
-   // in to that slot for the next operation.
-   node.left.num = node.left.has_human ? tracked_number : node.left.num;
-   node.right.num = node.right.has_human ? tracked_number : node.right.num; 
-   
-   let operation = _inverse[node.operation];
-   
-   //console.log(node.right.num, operation, node.left.num) 
-   
-   // I've reversed the operation here because I'm traversing the tree backwards with
-   // inverted operations. Doesn't matter for addition and multiplication, but for
-   // subtraction and division it does, or that's what I thought at least.
-   // For some reason it's returning very very wrong results with floating point
-   // numbers. I think my logic is sound, but I'm starting to doubt it though.
-   nr_to_track = eval(`${node.right.num} ${operation} ${node.left.num}`);
+   // Check which side of the node we need to evaluate next.
+   let side = node.left.has_human ? 'left' : 'right';
+
+   if (side === 'left') {
+      // If the tracked_number is substituted for the left number, we can just invert
+      // the operation and evaluate it against the right number in the same order as
+      // the original operation.
+      node.operation = _inverse[node.operation]
+      node.left.num = tracked_number;
+      tracked_number = eval(`${node.left.num} ${node.operation} ${node.right.num}`);
+   } else {
+      node.right.num = tracked_number;
+      
+      // If the tracked_number is substituted for the right number, we have to invert
+      // the whole operand, along with the operation, unless the operation is a
+      // subtraction, in which case we can just substitute the tracked_number for
+      // the right number and evaluate the operand in the same order as the original
+      if (node.operation !== '-') {
+         node.operation = _inverse[node.operation];
+         tracked_number = eval(`${node.right.num} ${node.operation} ${node.left.num}`);
+      } else {
+         tracked_number = eval(`${node.left.num} ${node.operation} ${node.right.num}`);
+      }
+   }
+
+
    
    // Recursively call the function with the next node and the new tracked number.
-   return track_human_path(humn_side, nr_to_track);
-}
-                        ///////////////////////////////
-                        ////// PROBLEM AREA ENDS //////
-                        ///////////////////////////////
-
-
-// Same thing as part 1, but instead of only tracking the number values on the 
-// tree, we also track the path of the human and pass the whole object back up
-// the recursion stack. Then we call track_human_path() with the tree object
-// and the number to match. This function will then recursively traverse the
-// tree back to humn and reverse the operations to get the number that humn
-// would have had to enter to get to the root node.
-function solve_part2(input, node_name) {
-   let curr_line = input[input.findIndex(line => line[0] === node_name)];
-   let [num, operation, left_name, right_name] = [-Infinity, '', '', ''];
-
-   if (curr_line.length == 2) num = Number(curr_line[1]);
-   else [operation, left_name, right_name] = [curr_line[2], curr_line[1], curr_line[3]];
-
-   let node = {
-      name: node_name, 
-      num: num,
-      operation: operation,
-      left: {name: left_name},
-      right: {name: right_name},
-      has_human: false
-   };
-   
-   if (node.num > -Infinity) {
-      if (node.name === 'humn') node.has_human = true;
-      return node;
-   } else {
-      node.left = solve_part2(input, node.left.name);
-      node.right = solve_part2(input, node.right.name);
-      //console.log(node.left, operation, node.right)
-      node.num = Number(eval(`${node.left.num} ${operation} ${node.right.num}`));
-      if (node.left.has_human || node.right.has_human) {
-         node.has_human = true;
-      }
-      
-      if (node.name === 'root') {
-         // Verified that the function returns the correct number for part 1 if I
-         // log the number here. So the problem is somewhere in the recursion that
-         // we do in track_human_path().
-
-         // We need to match the number of the node that doesn't contain humn.
-         let nr_to_match = node.left.has_human ? node.right.num : node.left.num;
-         let humn_branch = node.left.has_human ? node.left : node.right;
-         return track_human_path(humn_branch, nr_to_match);
-      }
-      return node
-   }
-}
-
-
-// Solved. Simple recursion to build a tree and evaluate it bottom-up
-function solve_part1(input, name) {
-   let tree = {};
-   let node = input[input.findIndex(line => line[0] === name)];
-   let [num, operation, left, right] = [-Infinity, '', '', ''];
-
-   if (node.length == 2) num = Number(node[1]);
-   else  [operation, left, right] = [node[2], node[1], node[3]];
-
-   tree[name] = {
-      num: num,
-      operation: operation,
-      left: left,
-      right: right
-   };
-   
-   if (tree[name].num > -Infinity) {
-      return tree[name].num;
-   } else {
-      tree[name].left = solve_part1(input, left);
-      tree[name].right = solve_part1(input, right);
-      console.log(tree[name].left, operation, tree[name].right)
-      tree[name].num = Number(eval(`${tree[name].left} ${operation} ${tree[name].right}`));
-      return tree[name].num
-   }
+   return track_human_path(humn_side, tracked_number);
 }
 
 // ----------------------------- Helper functions ----------------------------
